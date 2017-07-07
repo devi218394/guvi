2862aab08d909743920e17d03357942622239aae
devi218394 <devi218394@gmail.com>
2017-07-07 15:18:13 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,22 @@
+importjava.util.*;
+import java.lang.*;
+public class A
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String=n.next();
+char c[]=n.toCharArray();
+for(int i=0;i<c.length;i++)
+{
+if(Character.isLetter(c[i]);
+{
+System.out.print("the entered charachter is alphabet");
+}
+else
+{
+System.out.print("the entered character is not an alphabet ");
+}
+}
+}
+}
