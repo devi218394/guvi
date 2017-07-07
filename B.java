73f95cac8b97301a772eae77bbbd4e150bce2341
devi218394 <devi218394@gmail.com>
2017-07-07 15:36:19 +0530

    Create B.java

---
+++ B.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class B
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char c=sc.next().charAt(0);
+
+
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+{
+System.out.print("the entered charachter is vowel");
+}
+else
+{
+System.out.print("the entered character is not an constant ");
+}
+
+}
+}
