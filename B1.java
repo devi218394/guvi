5ea9f2888a54538ae0efc49986ad0cc87f32ac58
devi218394 <devi218394@gmail.com>
2017-07-07 17:16:34 +0530

    Create B1.java

---
+++ B1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+public class B1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int c=sc.nextInt()
+
+if(c%4==0)
+{
+System.out.print("leap year");
+}
+else
+{
+System.out.print("non-leap year");
+}
+
+}
+}
