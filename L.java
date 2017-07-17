389c879a1d09c966e4630379e35adbdda6d7f77e
devi218394 <devi218394@gmail.com>
2017-07-17 10:36:26 +0530

    Create L.java

---
+++ L.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class L
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n%4==0)
+{
+System.out.println("n is a leap year");
+}
+else
+{
+System.out.println("n is not  a leap year");
+}
+}
+}
