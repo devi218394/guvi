645ea7bdde7a7cb6896ef4e81b40488540110dbe
devi218394 <devi218394@gmail.com>
2017-07-18 23:33:10 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Odd
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int n1=sc.nextInt();
+for(int i=n;i<=n1;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i);
+}
+}
+}
+}
