8a9e8a98c07dedd69d036d8ac7defdcedda810c8
devi218394 <devi218394@gmail.com>
2017-07-23 17:47:24 +0530

    Create I.java

---
+++ I.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class I
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+int d=Integer.valueOf(s);
+System.out.print(d);
+}
+}
