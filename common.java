7f8e663f9451ac11e68462b6b8e53caaf9600206
devi218394 <devi218394@gmail.com>
2018-09-22 13:52:16 +0530

    Create common.java

---
+++ common.java
@@ -0,0 +1,15 @@
+import java.io.*
+class common
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String res=" ";
+int n=sc.nextInt();
+String a[]=new String[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.next();
+}
+for(int i=0;i<a.length;i++)
+{
